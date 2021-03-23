c5c46682d9eb94f40de68096cb4c187644ee738e
diablohs@naver.com <kimhs@innerwave.co.kr>
2021-03-24 08:09:17 +0900

    fix

--- src/ios/BeaconPlugin.swift
+++ src/ios/BeaconPlugin.swift
@@ -59,11 +59,17 @@ import UIKit
         locationFlag = true;
         bluetoothFlag = true;
         
-        oAuthDomain = "https://dev-oauth.indoorplus.io"; // oAuth 인증서버
-        apiDomain = "http://3.35.182.33:8401"; // api 서버
+        // oAuthDomain = "https://dev-oauth.indoorplus.io"; // oAuth 인증서버
+        // apiDomain = "http://3.35.182.33:8401"; // api 서버
+        // client_id = "cmcep";
+        // client_secret = "4c0d875631284405c82523523885effe";
+        // scope = "706E7430-F5F8-466E-AFF9-25556B571111";
+
+        oAuthDomain = "https://epauth.cmcnu.or.kr"; // oAuth 인증서버
+        apiDomain = "https://epapi.cmcnu.or.kr"; // api 서버
         client_id = "cmcep";
         client_secret = "4c0d875631284405c82523523885effe";
-        scope = "706E7430-F5F8-466E-AFF9-25556B571111";
+        scope = "636D632D-6575-6E70-7965-6F6E67502654";
         
         pnTADManager = PnTVestigoManager.sharedInstance();
         
